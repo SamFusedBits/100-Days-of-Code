5689742f559b9a1072c002725c8023d86c49d4e7
Sahil Memon <sahil.infosphere@gmail.com>
2024-03-25 23:59:23 +0530

    Initial Commit
    Update log.md

--- src/methodOverriding.java
+++ src/methodOverriding.java
@@ -42,7 +42,7 @@ public class methodOverriding {
             System.out.println("Choose a player: 1 - Warrior, 2 - Wizard, 3 - Archer");
             int choice = scanner.nextInt();
 
-/*            Player player = null;
+        /*   Player player = null;
 
             switch (choice) {
                 case 1:
@@ -58,7 +58,7 @@ public class methodOverriding {
                     System.out.println("Invalid choice. Defaulting to Player.");
                     player = new Player();
                     break;
-            }*/
+            } */
 
             //Enhanced Switch Statement
             Player player = switch (choice) {
@@ -76,6 +76,65 @@ public class methodOverriding {
             scanner.close();
         }
     }
-}
-// Method Overriding allows a subclass to provide a specific implementation of a method
-// that is already defined in its superclass.
+/* Method Overriding allows a subclass to provide a specific implementation of a method
+ that is already defined in its superclass. */
+
+                            // Day 2 of Method Overriding
+//    https://www.hackerrank.com/challenges/java-method-overriding/problem?isFullScreen=true
+    public class Solution {
+
+        static class Sports {
+            String getName() {
+                return "Generic Sports";
+            }
+
+            void getNumberofTeamMembers() {
+                System.out.println("Each team has n players in Generic Sports");
+            }
+        }
+
+        static class Soccer extends Sports {
+            @Override
+            String getName() {
+                return "Soccer Class";
+            }
+
+            int getNum() {
+                return 11;
+            }
+
+            void getNumberofTeamMembers() {
+                System.out.println("Each team has " + getNum() + " players in " + getName());
+            }
+        }
+
+        public static void main(String[] args) {
+            Sports sports = new Sports();
+            Soccer soccer = new Soccer();
+            System.out.println(sports.getName());
+            sports.getNumberofTeamMembers();
+            System.out.println(soccer.getName());
+            soccer.getNumberofTeamMembers();
+        }
+    }
+
+//    https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem?isFullScreen=true
+        static class function{
+            void functs(){
+                System.out.println("Hello I am a motorcycle, I am a cycle with an engine.");
+            }
+        }
+        static class func extends function{
+            @Override
+            void functs(){
+                super.functs();
+                System.out.println("My ancestor is a cycle who is a vehicle with pedals.");
+            }
+        }
+
+        public static void main(String[] args) {
+            func functions = new func();
+            functions.functs();
+
+        }
+    }
