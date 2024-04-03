773404f171bd15b74bfda03d17763ec875533684
Sahil Memon <sahil.infosphere@gmail.com>
2024-04-03 20:28:27 +0530

    Initial Commit

--- src/multiThreading.java
+++ src/multiThreading.java
@@ -79,7 +79,7 @@ public class multiThreading {
             orders.add("Order 2");
             orders.add("Order 3");
             orders.add("Order 4");
-            orders.add("Order 5");
+            orders.add  ("Order 5");
 
             // Process each order concurrently using threads
             for (String order : orders) {
@@ -87,3 +87,69 @@ public class multiThreading {
             }
         }
     }
+
+                            // Day 2 of MultiThreading
+/*    Synchronization in Java
+        Synchronization is used to control access to shared resources in a multithreaded environment.
+        It ensures that only one thread can access a shared resource at a time, preventing data corruption and race conditions.
+      Thread Communication and Collaboration in Java
+        Thread communication and collaboration involve multiple threads working together towards a common goal or
+        sharing information between each other. This can be achieved using techniques like inter-thread
+        communication, shared data structures, and synchronization. */
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    // Synchronized method for depositing money into the account
+    public synchronized void deposit(double amount) {
+        balance += amount;
+        System.out.println(Thread.currentThread().getName() + " deposited $" + amount);
+        notifyAll(); // Notify waiting threads after deposit
+    }
+
+    // Method for withdrawing money from the account
+    public synchronized void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println(Thread.currentThread().getName() + " withdrew $" + amount);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " insufficient balance for withdrawal");
+        }
+    }
+
+    // Method to transfer funds to another account
+    public synchronized void transfer(BankAccount recipient, double amount) {
+        while (amount > balance) {
+            try {
+                System.out.println(Thread.currentThread().getName() + " waiting for sufficient funds");
+                wait(); // Wait until sufficient funds available for transfer
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        withdraw(amount);
+        recipient.deposit(amount);
+        System.out.println(Thread.currentThread().getName() + " transferred $" + amount + " to " + recipient);
+    }
+
+    // Synchronized method to get the current balance
+    public synchronized double getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(2000);
+
+        // Create threads for transferring funds between accounts
+        Thread transferThread1 = new Thread(() -> account1.transfer(account2, 500), "Transfer Thread 1");
+        Thread transferThread2 = new Thread(() -> account2.transfer(account1, 700), "Transfer Thread 2");
+
+        transferThread1.start();
+        transferThread2.start();
+    }
+}
