f3bbd7df393fe57efe173fb12e6d616b047eb707
Sahil Memon <sahil.infosphere@gmail.com>
2024-04-07 20:00:44 +0530

    Update Day2

--- src/Patterns.java
+++ src/Patterns.java
@@ -44,4 +44,49 @@ public class Patterns {
         }
     }
 
+                                    //Day2 of Patterns
+
+    public static class XSquarePattern {
+        public static void main(String[] args) {
+            int size = 5;
+
+            // upper triangle
+            for (int i = 1; i <= size; i++) {
+                for (int j = 1; j <= size; j++) {
+                    if (j == i || j == size - i + 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+            // lower triangle
+            for (int i = size - 1; i >= 1; i--) {
+                for (int j = 1; j <= size; j++) {
+                    if (j == i || j == size - i + 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static class PascalTriangle {
+        public static void main(String[] args) {
+            int rows = 5;
+
+            for (int i = 0; i < rows; i++) {
+                int number = 1;
+                System.out.printf("%" + (rows - i) * 2 + "s", "");
+                for (int j = 0; j <= i; j++) {
+                    System.out.printf("%4d", number);
+                    number = number * (i - j) / (j + 1);
+                }
+                System.out.println();
+            }
+        }
+    }
 }
