be11752ff906a5222e7fb0bb61a6d4afe1417851
Sahil Memon <sahil.infosphere@gmail.com>
2024-04-07 20:31:37 +0530

    Update Day3 of patterns in java

--- src/Patterns.java
+++ src/Patterns.java
@@ -89,4 +89,66 @@ public class Patterns {
             }
         }
     }
+
+                            //Day3 of Patterns in Java
+    public static class AlphabetPattern {
+        public static void main(String[] args) {
+            int rows = 5;
+
+            for (int i = 0; i < rows; i++) {
+                char alphabet = 'E';
+                for (int j = 0; j <= i; j++) {
+                    System.out.print((char)(alphabet - j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static class NumberPattern {
+        public static void main(String[] args) {
+            int n = 4;
+            int rows = 2 * n - 1;
+
+            // Upper half
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < rows; j++) {
+                    int num = Math.max(Math.max(i, j), Math.max(rows - 1 - i, rows - 1 - j)) + 1;
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static class IncDecNumbersPattern {
+        public static void main(String[] args) {
+            int n = 4;
+
+            // Upper half of the pattern
+            for (int i = 0; i < n; i++) {
+                // For Spaces before numbers
+                for (int j = 0; j < i; j++) {
+                    System.out.print("  ");
+                }
+
+                // For Numbers in ascending order
+                for (int j = 1; j <= i + 1; j++) {
+                    System.out.print(j);
+                }
+
+                // For Spaces between numbers
+                for (int j = 0; j < 2 * (n - i - 1); j++) {
+                    System.out.print(" ");
+                }
+
+                // For Numbers in descending order
+                for (int j = i + 1; j > 0; j--) {
+                    System.out.print(j);
+                }
+
+                System.out.println();
+            }
+        }
+    }
 }
